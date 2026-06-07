b8c1bb08ba5937508422a50b0c4a21f106e16401
ming <guoqiming6661@gmail.com>
2026-06-07 19:33:52 +0800

    lcr

--- lcr/lcr-019.py
+++ lcr/lcr-019.py
@@ -1,11 +1,15 @@
 class Solution:
     def validPalindrome(self, s: str) -> bool:
-        i,j = 0,len(s)-1
+        i, j = 0, len(s) - 1
         while i < j:
             if s[i] != s[j]:
-                
-    def check(self,i: int,j : int,s : str) -> bool:
-        while(i < j):
+                return self.check(i + 1, j, s) or self.check(i, j - 1, s)
+            i += 1
+            j -= 1
+        return True
+
+    def check(self, i: int, j: int, s: str) -> bool:
+        while (i < j):
             if s[i] != s[j]:
                 return False
             i += 1
